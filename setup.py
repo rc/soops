8f53ac6a1ee96bbc3e14f15c5345cb94f59d2e4e
Robert Cimrman <cimrman3@ntc.zcu.cz>
2020-03-09 22:26:04 +0100

    new setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+Installation file for soops.
+"""
+import os
+from setuptools import setup
+
+import soops.version as version
+
+srcdir = os.path.dirname(__file__)
+
+readme_filename = os.path.join(srcdir, 'README.rst')
+
+setup(
+    name='soops',
+    version=version.__version__,
+    description='Run parametric studies and scoop output files.',
+    long_description=open(readme_filename, encoding="utf-8").read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='run parametric studies, scoop output',
+    url='http://github.com/rc/soops',
+    author='Robert Cimrman',
+    author_email='cimrman3@ntc.zcu.cz',
+    license='BSD',
+    packages=['soops'],
+    install_requires=[
+        'dask',
+        'pandas',
+        'pyparsing',
+    ],
+    entry_points={
+        'console_scripts': [
+            'soops-run=soops.run_parametric:main',
+            'soops-scoop=soops.scoop_outputs:main',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
