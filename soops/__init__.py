0637fcefbb8cf11a039564e9cad617582215528c
Robert Cimrman <cimrman3@ntc.zcu.cz>
2020-04-28 16:34:08 +0200

    update soops/__init__.py to include __version__

--- soops/__init__.py
+++ soops/__init__.py
@@ -1,3 +1,4 @@
 from .base import output, ordered_iteritems, import_file, shell, Struct
-from .parsing import parse_as_list, parse_as_dict
 from .ioutils import ensure_path, fix_path, locate_files, save_options
+from .parsing import parse_as_list, parse_as_dict
+from .version import __version__
