621c8994507f20ceb0ed12afb5201b42cdf40792
Robert Cimrman <cimrman3@ntc.zcu.cz>
2020-11-18 14:29:38 +0100

    update soops/__init__.py

--- soops/__init__.py
+++ soops/__init__.py
@@ -1,5 +1,5 @@
 from .base import (output, ordered_iteritems, import_file, shell, debug,
-                   debug_on_error, Struct)
+                   debug_on_error, run_command, Struct)
 from .ioutils import ensure_path, fix_path, locate_files, save_options
 from .parsing import parse_as_list, parse_as_dict
 from .version import __version__
