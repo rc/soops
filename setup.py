d7a7567cb9ac52fde4201e18499a998694df2672
Robert Cimrman <cimrman3@ntc.zcu.cz>
2021-03-13 16:16:43 +0100

    move setup.py data into new setup.cfg

--- setup.py
+++ setup.py
@@ -1,52 +1,9 @@
+#!/usr/bin/env python
 """
-Installation file for soops.
+This file is only required for editable installs, the project configuration
+is in setup.cfg.
 """
-import os
-from setuptools import setup
+import setuptools
 
-srcdir = os.path.dirname(__file__)
-readme_filename = os.path.join(srcdir, 'README.rst')
-
-def read_version_py(filename='soops/version.py'):
-    ns = {}
-    with open(filename, 'rb') as fd:
-        exec(fd.read(), ns)
-    return ns['__version__']
-
-version = read_version_py()
-
-setup(
-    name='soops',
-    version=version,
-    description='Run parametric studies and scoop output files.',
-    long_description=open(readme_filename, encoding="utf-8").read(),
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-    ],
-    keywords='run parametric studies, scoop output',
-    url='https://github.com/rc/soops',
-    author='Robert Cimrman',
-    author_email='cimrman3@ntc.zcu.cz',
-    license='BSD',
-    packages=['soops'],
-    python_requires='>=3.5',
-    install_requires=[
-        'pyparsing',
-        'dask',
-        'distributed',
-        'pandas',
-        'tables',
-        'matplotlib',
-    ],
-    entry_points={
-        'console_scripts': [
-            'soops-run=soops.run_parametric:main',
-            'soops-scoop=soops.scoop_outputs:main',
-            'soops-info=soops.print_info:main',
-        ],
-    },
-    include_package_data=True,
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setuptools.setup()
