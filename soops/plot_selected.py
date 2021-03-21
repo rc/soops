76541484a9f6e05f22afa80d20e33e54af44dd25
Robert Cimrman <cimrman3@ntc.zcu.cz>
2021-03-21 18:59:03 +0100

    fix get_legend_items() for both marker and fillstyle/markersize in key styles

--- soops/plot_selected.py
+++ soops/plot_selected.py
@@ -136,7 +136,8 @@ def get_legend_items(selected, styles, used=None, format_labels=None):
             kw = {}
             for skey, style_vals in key_styles.items():
                 kw[skey] = style_vals[iv % len(style_vals)]
-                if skey in ('fillstyle', 'markersize'):
+                if ((skey in ('fillstyle', 'markersize')) and
+                    ('marker' not in key_styles)):
                     kw['marker'] = 'o'
 
             if not len(kw):
